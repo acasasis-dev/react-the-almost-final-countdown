801cec18b1bc944a0eea6a15074d1a0fb3c49087
acasasis-dev <angelito.casasis@gmail.com>
2025-05-06 01:30:41 +0800

    Initial look of the game dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
+import TimerChallengeProps from './interfaces/TimerChallengeProps';
 
 function App() {
+  const challenges: TimerChallengeProps[] = [
+    {title: "Easy", targetTime: 1},
+    {title: "Not Easy", targetTime: 5},
+    {title: "Getting tough", targetTime: 10},
+    {title: "Pros only", targetTime: 15}
+  ]
+
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        {challenges.map(challenge => {
+          return <TimerChallenge title={challenge.title} targetTime={challenge.targetTime} />
+        })}
+      </div>
     </>
   );
 }
---
+++ src/interfaces/TimerChallengeProps.tsx
@@ -0,0 +1,4 @@
+export default interface TimerChallengeProps {
+	title: string,
+	targetTime: number
+}
